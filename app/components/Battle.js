f31fa5841e97e326fac883f583a8175cfbb7bf1d
prernalele <prernalele@gmail.com>
2021-02-05 06:37:33 -0500

    finishing github-battle project

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -2,78 +2,83 @@ import React from 'react'
 import { FaUserFriends, FaFrighterJet, FaTrophy, FaFighterJet , FaTimesCircle} from 'react-icons/fa'
 import PropTypes from 'prop-types'
 import Results from './Results'
+import { ThemeConsumer } from '../contexts/theme'
+import { Link } from 'react-router-dom'
 
 
 function Instructions() {
-
     return (
-        <div className='instructions-container'>
-            <h1 className='center-text header-lg'>
-            Instructions
-            </h1>
-            <ol className='container-sm grid center-text battle-instructions'>
-            <li>
-                <h3 className='header-sm'>Enter two Github users</h3>
-                <FaUserFriends className='bg-light' color='rgb(255,191,116)' size={140} />
-            </li>
-            <li>
-                <h3 className='header-sm'>Battle</h3>
-                <FaFighterJet className='bg-light' color='#727272' size={140} />
-            </li>
-            <li>
-                <h3 className='header-sm'>Battle</h3>
-                <FaTrophy className='bg-light' color='rgb(255,215,0)' size={140} />
-            </li>
-            </ol>
-        </div>
+        <ThemeConsumer>
+            {( {theme } )=>(
+
+            <div className='instructions-container'>
+                <h1 className='center-text header-lg'>
+                Instructions
+                </h1>
+                    <ol className='container-sm grid center-text battle-instructions'>
+                        <li>
+                            <h3 className='header-sm'>Enter two Github users</h3>
+                            <FaUserFriends className={`bg-${theme}`} color='rgb(255,191,116)' size={140} />
+                        </li>
+                        <li>
+                            <h3 className='header-sm'>Battle</h3>
+                            <FaFighterJet className='bg-light' color='#727272' size={140} />
+                        </li>
+                        <li>
+                            <h3 className='header-sm'>Battle</h3>
+                            <FaTrophy className='bg-light' color='rgb(255,215,0)' size={140} />
+                        </li>
+                    </ol>
+            </div>
+
+            ) }
+        </ThemeConsumer>
     )
 }
 
 class PlayerInput extends React.Component {
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            username : ''   
-        }
-        this.handleSubmit = this.handleSubmit.bind(this)
-        this.handleChange = this.handleChange.bind(this)
+    state = {
+        username : '' 
     }
-    handleSubmit(event) {
+
+    handleSubmit = (event) => {
         event.preventDefault()
         this.props.onSubmit(this.state.username)
     }
-    handleChange(event) {
+    handleChange = (event) => {
         this.setState({
             username:event.target.value
         })
     }
     render() {
         return (
-            <form className='column player' onSubmit={this.handleSubmit}>
-            <label htmlFor='username' className='player-label'>
-            {this.props.label}
-            </label>
-            <div className='row player-inputs'>
-                <input
-                    type='text'
-                    id='username'
-                    className='input-light'
-                    placeholder='github-username'
-                    autoComplete='off'
-                    value = {this.state.username}
-                    onChange={this.handleChange}
-                />
-                <button
-                className='btn dark-btn'
-                type='submit'
-                disabled={!this.state.username}>
-                Submit
-                </button>
-            </div>
-            </form>
+            <ThemeConsumer>
+                {( { theme })=>(
+                    <form className='column player' onSubmit={this.handleSubmit}>
+                        <label htmlFor='username' className='player-label'>
+                        {this.props.label}
+                        </label>
+                        <div className='row player-inputs'>
+                            <input
+                                type='text'
+                                id='username'
+                                className={`input-${theme}`}
+                                placeholder='github-username'
+                                autoComplete='off'
+                                value = {this.state.username}
+                                onChange={this.handleChange}
+                            />
+                            <button
+                            className={`btn ${theme === 'dark' ? 'light-btn': 'dark-btn'}`}
+                            type='submit'
+                            disabled={!this.state.username}>
+                            Submit
+                            </button>
+                        </div>
+                    </form>
+                )}
+            </ThemeConsumer>
         )
-
     }
 }
 
@@ -85,26 +90,33 @@ PlayerInput.proptypes = {
 function PlayerPreview ( { username, onReset, label }) {
 
     return (
-        <div className='column player'>
-            <h3 className='player-label'>{label}</h3>
-            <div className = 'row bg-light'>
-                <div className='player-info'>
-                    <img 
-                    className='avatar-small'
-                    src={`https://github.com/${username}.png?size=200`}
-                    alt={`Avatar for ${username}`}
-                    />
-                    <a 
-                        href={`https://github.com/${username}`}
-                        className = 'link'>
-                            {username}
-                    </a>
+        <ThemeConsumer>
+            { ( {theme })=> (
+
+                <div className='column player'>
+                <h3 className='player-label'>{label}</h3>
+                <div className = {`row bg-${theme}`} >
+                    <div className='player-info'>
+                        <img 
+                        className='avatar-small'
+                        src={`https://github.com/${username}.png?size=200`}
+                        alt={`Avatar for ${username}`}
+                        />
+                        <a 
+                            href={`https://github.com/${username}`}
+                            className = 'link'>
+                                {username}
+                        </a>
+                    </div>
+                    <button className='btn-clear flex-center' onClick={onReset}>
+                        <FaTimesCircle color='rgb(194, 57, 42)' size={26} />
+                    </button>
                 </div>
-                <button className='btn-clear flex-center' onClick={onReset}>
-                    <FaTimesCircle color='rgb(194, 57, 42)' size={26} />
-                </button>
-            </div>
-        </div>
+                </div>
+
+            )}
+
+        </ThemeConsumer>
     )
 
 }
@@ -117,26 +129,19 @@ PlayerPreview.proptypes = {
 
 
 export default class Battle extends React.Component {
-    constructor(props) {
-        super(props)
 
-        this.state = {
-            playerOne: null,
-            playerTwo: null,
-            battle: false
-        }
-
-        this.handleSubmit = this.handleSubmit.bind(this)
-        this.handleReset = this.handleReset.bind(this)
+    state = {
+        playerOne: null,
+        playerTwo: null
     }
 
-    handleSubmit(id, player) {
+    handleSubmit = (id, player) => {
         this.setState({
             [id]: player
         })
     }
 
-    handleReset(id) {
+    handleReset = (id)=> {
         this.setState({
             [id]:null
         })
@@ -145,13 +150,7 @@ export default class Battle extends React.Component {
 
     render(){
 
-        const { playerOne, playerTwo , battle } = this.state
-
-        if (battle === true) {
-
-            return <Results playerOne={playerOne} playerTwo={playerTwo} />
-
-        }
+        const { playerOne, playerTwo  } = this.state
         return (
             <React.Fragment>
                 <Instructions />
@@ -184,13 +183,15 @@ export default class Battle extends React.Component {
                     </div>
                      
                      {playerOne && playerTwo && (
-                         <button 
+                         <Link 
                          className='btn dark-btn btn-space'
-                         onClick={()=> this.setState({
-                             battle:true
-                         })}>
+                         to={{
+                             pathname: '/battle/results',
+                             search: `?playerOne=${playerOne}&playerTwo=${playerTwo}`
+                         }}
+                         >
                          Battle
-                         </button>
+                         </Link>
                      )}
 
                 </div>
--- app/components/Popular.js
+++ app/components/Popular.js
@@ -2,6 +2,9 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { fetchPopularRepos } from '../utils/api'
 import {FaUser, FaStar, FaCodeBranch, FaExclamationTriangle } from 'react-icons/fa'
+import Card from './Card'
+import Loading from './Loading'
+import Tooltip from './Tooltip';
 
 function LanguagesNav ({selected, onUpdateLanguage }) {
     const languages = ['All', 'Javascript', 'Ruby', 'Java', 'css', 'Python']
@@ -35,22 +38,19 @@ function ReposGrid ( { repos } ) {
                 const {name, owner, html_url, stargazers_count, forks, open_issues } = repo
                 const { login , avatar_url} = owner
                 return (
-                    <li key={html_url} className='repo bg-light'>
-                        <h4 className ='header-lg center-text'>
-                        #{index +1}
-                        </h4>
-                        <img 
-                            className = 'avatar'
-                            src={ avatar_url}
-                            alt={`Avatar for ${login}`}
-                        />
-                        <h2 className='center-text'>
-                        <a className='link' href={html_url}>{login}</a>
-                        </h2>
+                    <li key={html_url} >
+                        <Card
+                            header={`#${index +1}`}
+                            avatar={avatar_url}
+                            href={html_url}
+                            name={login}
+                        >   
                         <ul className='card-list'>
                             <li>
-                                <FaUser color='rgb(255,191,116)' size={22}/>
-                                <a href={`https://github.com/${login}`}> {login} </a>
+                                <Tooltip text="Github Username">
+                                    <FaUser color='rgb(255,191,116)' size={22}/>
+                                    <a href={`https://github.com/${login}`}> {login} </a>
+                                </Tooltip>
                             </li>
                             <li>
                                 <FaStar color='rgb(255,215,0)' size={22} />
@@ -69,6 +69,7 @@ function ReposGrid ( { repos } ) {
                             </li>
 
                         </ul>
+                        </Card>
                     </li>
                 )
             })}
@@ -81,23 +82,18 @@ ReposGrid.propTypes = {
 }
 
 export default class Popular extends React.Component {
-    constructor(props) {
-        super(props)
 
-        this.state = {
-            selectedLanguage : 'All',
-            repos: {},
-            error: null
-        }
-        this.updateLanguage = this.updateLanguage.bind(this)
-        this.isLoading = this.isLoading.bind(this)
+    state = {
+        selectedLanguage : 'All',
+        repos: {},
+        error: null
     }
 
     componentDidMount () {
         this.updateLanguage(this.state.selectedLanguage)
     }
 
-    updateLanguage (selectedLanguage) {
+    updateLanguage  = (selectedLanguage) =>{
         this.setState({
             selectedLanguage,
             error: null,
@@ -126,7 +122,7 @@ export default class Popular extends React.Component {
         }
     }
 
-    isLoading() {
+    isLoading = () => {
 
         const { selectedLanguage, repos, error } = this.state
 
@@ -143,9 +139,9 @@ export default class Popular extends React.Component {
                 selected = {selectedLanguage}
                 onUpdateLanguage = {this.updateLanguage}/>
 
-                {this.isLoading()&& <p>LOADING </p>}
+                {this.isLoading()&& <Loading text='Fetching Repos'/>}
 
-                {error && <p>{error}</p> }
+                {error && <p className='center-text error'>{error}</p> }
 
                 {repos[selectedLanguage] && <ReposGrid repos={repos[selectedLanguage]} />}
             </React.Fragment>
